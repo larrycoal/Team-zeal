a787bfefa201a0368d61ca313b0347e0127e5126
Olayiwola Olanrewaju <larry_coal@outlook.com>
2019-09-17 13:11:41 +0100

    view password functionality added

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function myFunction() {
       x.className = "nav-div";
     }
   }
+ //i apologise for the terrible variable names
+  var Vpass = document.getElementById("view")
+  var Pfield =document.getElementById("password")
+ 
+
+
+  function togglepassword(){
+    if (Pfield.type ="password"){
+      Pfield.type ="text";
+    }else{
+      Pfield.type="password"
+    }
+  }
+
+  Vpass.addEventListener("click",function(){
+     togglepassword();
+  })
+ 
