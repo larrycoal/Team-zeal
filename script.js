a827c129614932bd1aa3c85c79c4177367c9b50e
Nefe Emadamerho <nefejames@yahoo.com>
2019-09-16 21:12:26 -0700

    setting up form validation

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 function myFunction() {
-    var x = document.getElementById("nav-div");
-    if (x.className === "nav-div") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-div";
-    }
+  var x = document.getElementById("nav-div");
+  if (x.className === "nav-div") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-div";
   }
+}
+
+function validate() {
+  var name = document.getElementById("name").value;
+  var password = document.getElementById("password").value;
+  var error_message = document.getElementById("error_message");
+
+  var text;
+  if (name.length < 4) {
+    text = "Name too short. Please enter a valid Name";
+    error_message.style.display = "inline-block";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if (password.length < 8) {
+    text = "Password is too short. Please enter a valid password";
+    error_message.style.display = "inline-block";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  alert("Form Submitted Successfully!");
+  return true;
+}
